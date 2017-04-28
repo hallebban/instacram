e63cc122abf43f1068f37cb97c08d37f532b766a
Raghu Betina <raghu@firstdraft.com>
2017-04-28 00:33:37 +0000

    Direct association between users and followers

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,6 +1,9 @@
 class Follower < ApplicationRecord
   # Direct associations
 
+  belongs_to :sender,
+             :class_name => "User"
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :followers,
+             :foreign_key => "sender_id",
+             :dependent => :destroy
+
   has_many   :likes,
              :dependent => :destroy
 
